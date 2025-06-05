25fd8185eda3b89ae943ffd2c36b248a9697e7b4
Zenmaster911 <mazdanmazdanov@gmail.com>
2025-06-05 11:04:53 +0300

    db integration

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	todo "github.com/zenmaster911/shelfAPI"
 	"github.com/zenmaster911/shelfAPI/pkg/handler"
 	"github.com/zenmaster911/shelfAPI/pkg/repository"
@@ -10,13 +11,22 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatal("error in initialization configs: %v", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatal("error occured while running http server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
